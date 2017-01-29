0429359ea88fac62d318721c90bb0838ad101ddb
Sammy Wong <19537335+sw-uci@users.noreply.github.com>
2017-01-29 01:32:01 -0800

    Add ytthumbnail.js

---
+++ ytthumbnail.js
@@ -0,0 +1,1 @@
+javascript:if (document.URL.match(/https:\/\/www.youtube.com\/watch\?v=/)) {window.open("https://i.ytimg.com/vi/"+document.URL.match(/v=([^?&=]+)/)[1]+"/hqdefault.jpg", "_blank");}void(0);
